1422c4718e2c49cbfe7367abdf5ea8f433396345
Namani <kwg531@naver.com>
2024-08-04 21:26:19 +0900

    데이터바인딩 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="./assets/logo.png" alt="Vue logo">
+
+    <h2>{{products[0]}}</h2>
+    <p> 생선: 3만원 입니다.</p>
+  </div>
+  <div>
+    <h2>{{products[1]}}</h2>
+    <p>고기 : 2만원 입니다</p>
+  </div>
+  <div>
+    <h2>{{products[2]}}</h2>
+    <p>과일 : 4만원 입니다.</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data() {
+    return {
+      products: ['광장시장', '한민시장', '구경시장']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>

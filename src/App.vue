5605c9fe4e22ba73f10d801ec5c6117f333e1281
Namani31 <kwg531@naver.com>
2024-08-05 17:14:41 +0900

    Feat: `v-for`를 이용하여 `<div>` 3개를 반복문으로 축약

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div>
-    <img src="./assets/logo.png" alt="Vue logo">
-
-    <h2>{{products[0]}}</h2>
-    <p> 생선: 3만원 입니다.</p>
+  <div class="menu">
+    <a v-for="navMenu in menu" :key="navMenu">{{ navMenu }}</a>
   </div>
   <div>
-    <h2>{{products[1]}}</h2>
-    <p>고기 : 2만원 입니다</p>
+    <img src="./assets/logo.png" alt="Vue logo" />
   </div>
-  <div>
-    <h2>{{products[2]}}</h2>
-    <p>과일 : 4만원 입니다.</p>
+  <div v-for="(products, index) in products" :key="index">
+    <h2>{{ products }}</h2>
+    <p>50만원</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      products: ['광장시장', '한민시장', '구경시장']
-    }
+      menu: ["Home", "Shop", "About"],
+      products: ["광장시장", "한민시장", "구경시장"],
+    };
   },
-  components: {
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
@@ -36,5 +32,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
